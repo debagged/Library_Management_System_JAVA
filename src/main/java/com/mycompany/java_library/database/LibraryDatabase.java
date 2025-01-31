fdde67703f9c00a65f9ae64d80938897ff38d960
debagged <angeloflorendo.goloyugo@gmail.com>
2025-01-31 14:52:21 +0800

    eto na andrei

---
+++ src/main/java/com/mycompany/java_library/database/LibraryDatabase.java
@@ -0,0 +1,23 @@
+package com.mycompany.java_library.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LibraryDatabase {
+
+    public static void main(String[] args) {
+        // Corrected URL, username, and password
+        String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12759421?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String username = "sql12759421";
+        String password = "185FBnKxER";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected");
+            
+        } catch (SQLException e) {
+            System.err.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
